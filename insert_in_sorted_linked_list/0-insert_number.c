3d11a15bc8afbae6b0b73bd246f6805568fffc9d
Repli1 <aerisvelivolus22@hotmail.com>
2023-08-24 13:35:29 +0000

    FIx edgecases of beginning, end and empty list

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -1,27 +1,34 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * insert_node - Function that inserts a node in a sorted linked list.
- * @head: A pointer to the first node of the list.
- * @number: Number to insert in the node.
- * Return: The new node.
+ * insert_node - Function that inserts node in a sorted linked list
+ * @head: Pointer to the first node of the list
+ * @number: Number to insert in the node
+ * Return: The new node
  */
-
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *current;
 	listint_t *new = malloc(sizeof(listint_t));
 
+	if (*head == NULL)
+		return (NULL);
 	current = *head;
-
-	while (number > current->next->n)
+	if (number < current->n)
+	{
+		new->n = number;
+		new->next = *head;
+		*head = new;
+	}
+	while (current->next && number > current->next->n)
 	{
 		current = current->next;
 	}
 	new->n = number;
 	new->next = current->next;
 	current->next = new;
+
 	return (new);
 }
