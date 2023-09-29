d9aacff6fa67e716949b5794596481b253c3b5ab
Repli1 <cladescandor42@gmail.com>
2023-09-29 14:34:22 +0000

    Final test

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Log Parsing """
 
-
-while(1):
+codes_dict = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+              "404": 0, "405": 0, "500": 0}
+while True:
     total_size = 0
     line_counter = 0
     check = 0
