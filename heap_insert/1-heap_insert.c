e44d13b72363978aede28811497016d982f434ff
Repli1 <cladescandor42@gmail.com>
2023-10-10 11:46:28 +0000

    Add documentation

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -52,41 +52,63 @@ heap_t *heap_insert(heap_t **root, int value)
 	return (new);
 }
 
+/**
+ * get_height - Gets the height of a heap.
+ * @root: Pointer to the root of the heap.
+ * Return: 0 if NULL pointer otherwise height of the heap.
+ */
 int get_height(heap_t *root)
 {
 	if (root == NULL)
-		return 0;
+		return (0);
 	if (get_height(root->left) > get_height(root->right))
-		return get_height(root->left) + 1;
-	return get_height(root->right) + 1;
+		return (get_height(root->left) + 1);
+	return (get_height(root->right) + 1);
 }
 
+/**
+ * is_complete - Checks if heap is complete.
+ * @root: Pointer to the root node of the heap.
+ * Return: -1 if NULL pointer, 0 if not complete, 1 if complete.
+ */
 int is_complete(heap_t *root)
 {
 	int depth = 1;
 	heap_t *current = root;
 
 	if (root == NULL)
-		return -1;
+		return (-1);
 	while (current->right)
 	{
 		depth++;
 		current = current->right;
 	}
 	if (depth == get_height(root))
-		return 1;
-	return 0;
+		return (1);
+	return (0);
 }
 
+/**
+ * get_depth - Gets depth of a node.
+ * @node: Pointer to the node.
+ * Return: -1 if NULL pointer, otherwise depth of the node.
+ */
 int get_depth(heap_t *node)
 {
 	if (node == NULL)
-		return -1;
+		return (-1);
 	if (node->parent)
-		return get_depth(node->parent) + 1;
-	return 1;
+		return (get_depth(node->parent) + 1);
+	return (1);
 }
 
+/**
+ * search_parent - Searchs for the correct node to be the parent node.
+ * @root: Pointer to the root of the heap.
+ * @current: Moving pointer for recursion, pass root again on call.
+ * @buffer: Buffer to store the valid node for recursion (Pass NULL on call).
+ * Return: A pointer to the found node.
+ */
 heap_t *search_parent(heap_t *root, heap_t *current, heap_t *buffer)
 {
 	if (current->left && buffer == NULL)
@@ -98,17 +120,23 @@ heap_t *search_parent(heap_t *root, heap_t *current, heap_t *buffer)
 		buffer = search_parent(root, current->right, buffer);
 	}
 	if (check_node(root, current) == 1)
-		return current;
-	return buffer;
+		return (current);
+	return (buffer);
 }
 
+/**
+ * check_node - Checks if a node is valid for search_parent.
+ * @root: Pointer to the root node of the heap.
+ * @node: Node to check.
+ * Return: -1 if pointer is NULL, 0 if node is not valid, 1 if it is.
+ */
 int check_node(heap_t *root, heap_t *node)
 {
 	if (node == NULL)
-		return -1;
+		return (-1);
 	if (node->left && (node->right == NULL))
-		return 1;
+		return (1);
 	if (get_height(root) != get_depth(node) && !(node->left || node->right))
-		return 1;
-	return 0;
+		return (1);
+	return (0);
 }
